7cd08573fedede2e08a69810111af1406fafa2c6
Filip <filipporeba967@gmail.com>
2022-12-13 16:56:50 +0100

    upperCase function

--- main.c
+++ main.c
@@ -16,26 +16,38 @@ void zerosFill(int *data, int size) {
     }
 }
 
+void upperCase(char *text) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        char c = ((int) text[i]);
+        if (c >= 97 & c <= 122) {
+            text[i] = c - 32;
+        }
+    }
+    printf("\ntext = %s", text);
+}
+
 int main() {
-    int f = 74;
-    int e = 456;
-    swap(&f, &e);
-    printf("f = %i\n", f);
-    printf("e = %i\n", e);
-    printf("adres f = %u\n", &f);
-    printf("adres e = %u\n", &e);
-    int *p = &f;
-    printf("Wartosc p = %d\n", *p);
-    p = &e;
-    *p = 821;
-    printf("Wartosc p = %d\n", *p);
-    printf("Wartosc e = %d\n", e);
-    int g = add(&f, p);
-    printf("suma = %i\n", g);
-    int arr[] = {432, 7, 34, 12, 76, 90, 54};
-    int size = sizeof arr / 4;
-    zerosFill(arr, size);
-    printf("first = %i\n", arr[0]);
-    printf("last = %i\n", arr[size - 1]);
+//    int f = 74;
+//    int e = 456;
+//    swap(&f, &e);
+//    printf("f = %i\n", f);
+//    printf("e = %i\n", e);
+//    printf("adres f = %u\n", &f);
+//    printf("adres e = %u\n", &e);
+//    int *p = &f;
+//    printf("Wartosc p = %d\n", *p);
+//    p = &e;
+//    *p = 821;
+//    printf("Wartosc p = %d\n", *p);
+//    printf("Wartosc e = %d\n", e);
+//    int g = add(&f, p);
+//    printf("suma = %i\n", g);
+//    int arr[] = {432, 7, 34, 12, 76, 90, 54};
+//    int size = sizeof arr / 4;
+//    zerosFill(arr, size);
+//    printf("first = %i\n", arr[0]);
+//    printf("last = %i\n", arr[size - 1]);
+    char text[] = "Alamakota";
+    upperCase(text);
     return 0;
 }
